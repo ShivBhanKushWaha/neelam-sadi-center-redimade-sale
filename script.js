dd5d00a4abd5a4ac27973475bd6b1a43bdf0d974
Shivbhan Kushwaha <108090221+vishnabh@users.noreply.github.com>
2022-10-08 15:18:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,84 +4,105 @@ let product = {
             productName:"Rgular plane top Green color",
             category:"Topwear",
             price:"349",
+            discount:"15%",
             image:"green-top-wear.png"
+        },
+        {
+            productName:"Rgular jeans",
+            category:"Jeans",
+            price:"749",
+            discount:"15%",
+            image:"jeans.png"
         },
         {
             productName:"Regular white top ",
             category:"Topwear",
             price:"679",
+            discount:"18%",
             image:"white-top.png"
         },
         {
             productName:"Regular red top wear",
             category:"Topwear",
             price:"449",
+            discount:"13%",
             image:"red-top-wear.png"
         },
         {
             productName:"Regular top red color ",
             category:"Topwear",
             price:"249",
+            discount:"05%",
             image:"Red-top.png"
         },
         {
             productName:"Plazo bottom wear for girls",
             category:"Bottomwear",
             price:"579",
+            discount:"16%",
             image:"white-bottom-wear.png"
         },
         {
             productName:"Regular white cotton plazo",
             category:"Bottomwear",
             price:"879",
+            discount:"45%",
             image:"white-bottom.png"
         },
         {
             productName:"Regular bottom wear paint for girls",
             category:"Bottomwear",
             price:"479",
+            discount:"18%",
             image:"bottom-wear.png"
         },
         {
             productName:"Leather brown jacket",
             category:"Jacket",
             price:"839",
+            discount:"11%",
             image:"leather-jacket.png"
         },
         {
             productName:"Leather light Black jacket",
             category:"Jacket",
             price:"1139",
+            discount:"25%",
             image:"light-black-jacket.png"
         },
         {
             productName:"Leather red jacket",
             category:"Jacket",
             price:"1239",
+            discount:"15%",
             image:"red-jacket.png"
         },
         {
             productName:"Regular Strech Lower",
             category:"Lower",
             price:"829",
+            discount:"16%",
             image:"all-lower.png"
         },
         {
             productName:"Regular black Strech Lower",
             category:"Lower",
             price:"129",
+            discount:"25%",
             image:"black-lower.png"
         },
         {
             productName:"White Strech Lower",
             category:"Lower",
             price:"129",
+            discount:"35%",
             image:"white-cooton-lower.png"
         },
         {
             productName:"White and black lynon Strech Lower",
             category:"Lower",
             price:"229",
+            discount:"26%",
             image:"white-lower.png"
         }
     ],
@@ -118,6 +139,12 @@ for(let i of product.data){
     let price = document.createElement("h6");
     price.innerText = "₹" + i.price;
     container.appendChild(price);
+
+
+    // discount
+    let discount = document.createElement("h5");
+    discount.innerText ="Discount " + i.discount;
+    container.appendChild(discount);
 
 
     card.appendChild(container);
